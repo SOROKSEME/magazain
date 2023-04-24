4d24ff5d21b4ac237f58b4d069aeaa3c439c7111
student <student@intranet.proweb.uz>
2023-04-24 20:53:54 +0500

    models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Brand
+
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "pk")
+    list_display_links = ("title", "pk")
+    prepopulated_fields = {"slug": ("title", )}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug")
+    list_display_links = ("pk", "title")
