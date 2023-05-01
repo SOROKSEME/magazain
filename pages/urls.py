a687ce01caa422bc34effa994a6b9293a02b58d8
student <student@intranet.proweb.uz>
2023-05-01 20:57:56 +0500

    add product

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('shop/', views.shop_view, name='shop')
+    path('shop/', views.shop_view, name='shop'),
+    path("shop/categories/<slug:slug>/", views.category_products, name="category_products"),
+    path("shop/products/<slug:slug>/", views.product_detail, name="product_detail")
 ]
